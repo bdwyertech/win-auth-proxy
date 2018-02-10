79996591e3bb892c95d44d49f5e2f098f676b270
Qichao Zhao <qczhao@gmail.com>
2018-02-10 17:28:31 +0800

    updated to working version

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# Authentication-proxy
+# authentication-proxy
 
-[![Build status](https://ci.appveyor.com/api/projects/status/u0tbng5wockjgd97?svg=true)](https://ci.appveyor.com/project/IvoBellinSalarin/authentication-proxy)
+A simple Windows Authentication proxy.
 
-A simple Windows Authentication proxy. This application injects the current session's Kerberos token in the communication between a client software and a corporate proxy. The client software is unable to perform the Negotiate authentication exchanges and the corporate proxy only accepts authenticated users. Usually, the corporate proxy accepts NTLM and Negotiate as authentication protocols.
+This application injects the current session's Kerberos token in the communication between a client software and a corporate proxy.
 
-## Which is the use case, exactly ?
+The client software is unable to perform the Negotiate authentication exchanges and the corporate proxy only accepts authenticated users. Usually, the corporate proxy accepts NTLM and Negotiate as authentication protocols.
 
-Many package managers and source control managers are not able to perform a Negotiate exchange to authenticate the communication. This means that npm, git, docker, bower and so on will be unable to pass through a corporate proxy.
+## Which is the use case, exactly?
 
-Some tools, like CNTLM, allow you to pass your NTLM token to the proxy. This is a different protocol, less secure than Negotiate. CNTLM shall be configured writing your username/domain/encrypted password to a file.
-A patch for CNTLM allows you to use the Negotiate protocol (avoiding the need for a password saved in a file), but no binary is available nowadays. Moreover, in my personal  environment, CNTLM is *slow*: it won't be able to follow the rhythm of the exchanges between npm and the npm registry.
+Many package managers and source control managers are not able to perform a Negotiate exchange to authenticate the communication.
+
+This means that npm, git, docker, bower and so on will be unable to pass through a corporate proxy. Some tools, like CNTLM, allow you to pass your NTLM token to the proxy. This is a different protocol, less secure than Negotiate. CNTLM is configured by writing your username/domain/encrypted password to a file.
+
+A patch for CNTLM allows you to use the Negotiate protocol (avoiding the need for a password saved in a file), but no binary is available nowadays.
 
 ## Building
 
@@ -38,5 +41,6 @@ go get github.com/qichaozhao/authentication-proxy
 * You must run the application as user having a valid kerberos session ticket (i.e. log in with your corporate identity, then start this software)
 * Does not reply to mutual authentication request, but it's probably somewhat rare to bump into with web applications.
 * 64-bit platforms should still offer 32-bit compatible library/API so the application should compile and work. There's afaik no reason for which the application should be 64-bit.
-* The application does not add proxy headers, or manipulate any other headers besides Www-Authenticate/Authorization intentionally.
+* The application does not change any headers besides Proxy-Authorization intentionally.
 * Works only on Windows (because of the syscalls being called)
+* The application acts as a MITM proxy for the purposes of SSL connections, so you will need to disregard SSL certificate warnings (or install the self signed certificate) in order to use this tool. I would be welcome to any PR that allows this to seamlessly proxy SSL requests without certificate issues, but I haven't been able to figure this out myself (SSL is hard!)
--- gssapi-proxy.go
+++ gssapi-proxy.go
@@ -2,16 +2,16 @@ package main
 
 import (
 "fmt"
-"log"
 "syscall"
 "unsafe"
-"github.com/elazarl/goproxy"
+"log"
+"strings"
 "encoding/base64"
 "net/http"
-"strings"
-"os"
 "net/url"
-"crypto/tls"
+"github.com/elazarl/goproxy"
+"regexp"
+"os"
 )
 
 var (
@@ -39,7 +39,14 @@ var (
       0x00090312:  "SEC_I_CONTINUE_NEEDED",
       0x00090313:  "SEC_I_COMPLETE_NEEDED"}
 
-  )
+)
+
+func orPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 
 func AcquireCredentialsHandle(principal *uint16, pckge *uint16, credentialuse uint32, logonid *uint64, authdata *byte, getkeyfn *byte, getkeyargument *byte, credential *CredHandle, expiry *TimeStamp) (status SECURITY_STATUS) {
     r0, _, _ := syscall.Syscall9(procAcquireCredentialsHandleW.Addr(), 9, uintptr(unsafe.Pointer(principal)), uintptr(unsafe.Pointer(pckge)), uintptr(credentialuse), uintptr(unsafe.Pointer(logonid)), uintptr(unsafe.Pointer(authdata)), uintptr(unsafe.Pointer(getkeyfn)), uintptr(unsafe.Pointer(getkeyargument)), uintptr(unsafe.Pointer(credential)), uintptr(unsafe.Pointer(expiry)))
@@ -171,53 +178,76 @@ func HasNegotiateChallenge() goproxy.RespConditionFunc {
     }
 }
 
+func getAuthorizationHeader (proxyURL string) string {
+
+	// Acquire credentials
+	cred, status, err := AcquireCredentials("")
+	if err != nil {
+		log.Printf("AcquireCredentials failed: %v %s", err, errors[int64(status)])
+	}
+	defer cred.Close()
+	log.Printf("AcquireCredentials success: status=0x%x", status)
+
+	// Initialize Context
+	tgt := "http/" + strings.ToUpper(strings.Replace(strings.Split(proxyURL, ":")[1], "//", "", -1))
+	log.Printf("Requesting for context against SPN %s",tgt)
+	ctxt, status, err := cred.NewContext(tgt)
+	
+	if err != nil {
+		log.Printf("NewContext failed: %v", err)
+	}
+	log.Printf("NewContext success: status=0x%x errorcode=%s", status, errors[int64(status)]) 
+
+	// Generate the Authorization header
+	headerstr := "Negotiate " + base64.StdEncoding.EncodeToString(ctxt.Data[0:ctxt.Buffer.Count])
+	log.Printf("Generated header %s", headerstr)
+	
+	return headerstr
+}
+
+
 func main() {
-    
-    proxy := goproxy.NewProxyHttpServer()
-    proxy.Verbose = true
 
-    // transfer all the requests via the proxy specified on the command line as first positional argument
-    proxy.Tr = &http.Transport { Proxy: func (req *http.Request) (*url.URL, error) { return url.Parse(os.Args[1]) } }
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	
+	var auth_data string 
+	var AlwaysMitmAuth goproxy.FuncHttpsHandler = func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		// Initialise authentication data
+		auth_data = getAuthorizationHeader(os.Args[1])
 
-    proxy.OnResponse(HasNegotiateChallenge()).DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx)*http.Response {
-        ctx.Logf("Received 407 and Proxy-Authenticate from server, proceeding to reply")
-
-        // Acquire credentials
-        cred, status, err := AcquireCredentials("")
-        if err != nil {
-            ctx.Warnf("AcquireCredentials failed: %v %s", err, errors[int64(status)])
-        }
-        defer cred.Close()
-        ctx.Logf("AcquireCredentials success: status=0x%x", status)
-
-        // Initialize Context
-        tgt := "http/" + strings.ToUpper(strings.Replace(strings.Split(os.Args[1], ":")[1], "//", "", -1))
-        ctx.Logf("Requesting for context against SPN %s",tgt)
-        ctxt, status, err := cred.NewContext(tgt)
-        if err != nil {
-            ctx.Warnf("NewContext failed: %v", err)
-        }
-        ctx.Logf("NewContext success: status=0x%x errorcode=%s", status, errors[int64(status)]) 
-
-        // Generate the Authorization header
-        headerstr := "Negotiate " + base64.StdEncoding.EncodeToString(ctxt.Data[0:ctxt.Buffer.Count])
-        ctx.Logf("Generated header %s", headerstr)
-
-        // Modify the original request, and re run the request
-        ctx.Req.Header["Proxy-Authorization"] = []string{headerstr}
-        
-        // We need to make sure it goes through the proxy URL again
-        transport := &http.Transport { Proxy: func (req *http.Request) (*url.URL, error) { return url.Parse(os.Args[1]) } }
-        client := &http.Client{Transport: transport}
-
-        newr, err := client.Do(ctx.Req)
-        if err != nil {
-            ctx.Warnf("New request failed: %v", err)
-        }
-        ctx.Logf("Got response, forwarding it back to client")
-
-        // Return the new response in place of the original
-        return newr
-        })
-log.Fatal(http.ListenAndServe(":3128", proxy))
+		// Transfer all the requests via the proxy specified on the command line as first positional argument
+		proxy.Tr = &http.Transport {
+			Proxy: func (req *http.Request) (*url.URL, error) { return url.Parse(os.Args[1]) },
+			ProxyConnectHeader: http.Header{
+				"Proxy-Authorization": {auth_data},
+			},
+		}
+		return goproxy.MitmConnect, host
+	}
+	
+	// Handle HTTPS Connect Requests
+	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*"))).HandleConnect(AlwaysMitmAuth)
+	
+	// Handle HTTP Connect Requests
+	proxy.OnRequest(goproxy.Not(goproxy.ReqHostMatches(regexp.MustCompile("^.*:443$")))).
+		DoFunc(func (req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+				
+			// Initialise authentication data
+			auth_data = getAuthorizationHeader(os.Args[1])
+	
+			// Transfer all the requests via the proxy specified on the command line as first positional argument
+			proxy.Tr = &http.Transport {
+				Proxy: func (req *http.Request) (*url.URL, error) { return url.Parse(os.Args[1]) },
+				ProxyConnectHeader: http.Header{
+					"Proxy-Authorization": {auth_data},
+				},
+			}
+	
+			req.Header.Set("Proxy-Authorization", auth_data)
+			return req, nil
+	})
+	
+	log.Fatal(http.ListenAndServe(":3128", proxy))
+	
 }
